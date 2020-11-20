c188eaaaba4d725e633ff77d5cc1d0e373014177
Alan Parley <alanparley@hotmail.com>
2020-11-20 14:40:34 +0000

    set up Repo class

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Repository {
+
+    private String name;
+    private String description;
+    private ArrayList<Commit> commits;
+    //repo type?
+
+    public Repository(String name, String description){
+        this.name = name;
+        this.description = description;
+        this.commits = new ArrayList<Commit>();
+
+    }
 }
