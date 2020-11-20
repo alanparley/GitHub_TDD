89b170d195b4cc0c2ca0658148863b140a00cbf5
Alan Parley <alanparley@hotmail.com>
2020-11-20 14:29:26 +0000

    initial commit

---
+++ src/main/java/Commit.java
@@ -0,0 +1,11 @@
+public class Commit {
+
+    private String description;
+    private String uniqueId;
+
+    public Commit(String description, String uniqueId) {
+        this.description = description;
+        this.uniqueId = uniqueId;
+    }
+}
+
