1726b1946dbebff453ed17661a6b026270cc4aa1
Alan Parley <alanparley@hotmail.com>
2020-11-20 14:55:45 +0000

    set up commit, githubaccount and repo classes

--- src/main/java/GitHubAccount.java
+++ src/main/java/GitHubAccount.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class GitHubAccount {
+
+    private String name;
+    private String userName;
+    private ArrayList<Repository> repositories;
+    private String accountType;
+
+    public GitHubAccount(String name, String userName, String accountType){
+        this.name = name;
+        this.userName = userName;
+        this.repositories = new ArrayList<Repository>();
+        this.accountType = accountType;
+    }
+    
 }
--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -5,11 +5,12 @@ public class Repository {
     private String name;
     private String description;
     private ArrayList<Commit> commits;
-    //repo type?
+    private String repoType;
 
-    public Repository(String name, String description){
+    public Repository(String name, String description, String repoType){
         this.name = name;
         this.description = description;
+        this.repoType = repoType;
         this.commits = new ArrayList<Commit>();
 
     }
